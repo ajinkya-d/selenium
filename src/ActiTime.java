3dfe0540641b4b9fed63d2dc333fc8ac1b23a9fe
Lenovo <Lenovo@DESKTOP-NSDQ6U8>
2020-09-24 11:31:14 +0530

    3rd commit

---
+++ src/ActiTime.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ActiTime{ public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe");
+	ChromeDriver Driver = new ChromeDriver();
+	driver.manager().window().maximize();
+	driver.get("https://demo.actitime.com/login.do");
+	Thread.sleep(2000);
+	WebElement serachBox = driver
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+}
---
+++ src/SeleniumT.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumT {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe");
+		ChromeDriver Driver = new ChromeDriver();
+		
+
+	}
+
+}
